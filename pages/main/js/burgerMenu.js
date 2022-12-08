28a902a864dff7ca45d0ab41e3779dbd41895e66
darya-startsava <daryastartsava@gmail.com>
2022-12-08 10:04:25 +0100

    fix: close burger menu on overlay click

--- pages/main/js/burgerMenu.js
+++ pages/main/js/burgerMenu.js
@@ -3,6 +3,8 @@ openBurgerMenuButton.addEventListener('click', openBurgerMenu);
 const burgerMenuOverlay = document.querySelector('.nav__burger__menu__overlay');
 const closeBurgerMenuButton = document.querySelector('.nav__burger__menu__close-button');
 closeBurgerMenuButton.addEventListener('click', closeBurgerMenu);
+const burgerMenuPopup = document.querySelector('.nav__burger__menu__wrapper');
+burgerMenuOverlay.addEventListener('click', closeBurgerMenu);
 
 function openBurgerMenu() {
   openBurgerMenuButton.classList.add('hide');
@@ -10,6 +12,12 @@ function openBurgerMenu() {
 }
 
 function closeBurgerMenu() {
+  if (
+    event.target.closest('.nav__burger__menu__wrapper') &&
+    !event.target.closest('.nav__burger__menu__close-button')
+  ) {
+    return;
+  }
   openBurgerMenuButton.classList.remove('hide');
   burgerMenuOverlay.classList.remove('show');
 }
